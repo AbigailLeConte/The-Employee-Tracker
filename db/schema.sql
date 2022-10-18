876a000341186106d6bc23dd258c9d103ccd1801
Abigail LeConte <abigailleconte@Abigails-MBP.attlocal.net>
2022-10-17 21:02:17 -0400

    editing code

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department_db (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+)
+CREATE TABLE role_db (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  salary_id DEC,
+  department_id INT,
+)
+CREATE TABLE employee_db (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  department_id INT,
+)
+
+--need to format code to project 12--
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO employee_db (name)
+VALUES ("Sales Person"),
+       ("Lead Engineer"),
+       ("Software Engineer"),
+       ("Account Manager");
+       ("Accountant");
+       ("Legal Team Leed");
+       ("Lawyer");
+
+INSERT INTO manager_db (name, department)
+VALUES ("Intro to JavaScript", 1),
+       ("Data Science", 2),
+       ("Linear Algebra", 3),
+       ("History of the Internet", 4),
+       ("Machine Learning", 4),
+       ("Game Design", 1 ),
+       ("Cloud Development", 1);
+       
+
+
+-- need to write out code for project requirments --
